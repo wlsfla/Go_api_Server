8950e07f376b5a7c8081a1b5677134d1a44eec9e
wlsfla <lord8333@gmail.com>
2022-12-19 16:36:13 +0900

    update Fiber Framework

--- src/app/app.go
+++ src/app/app.go
@@ -13,7 +13,7 @@ import (
 func main() {
 	app := fiber.New()
 
-	file, err := os.OpenFile("result.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile("./log/access.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
@@ -22,17 +22,21 @@ func main() {
 	app.Use(requestid.New())
 	app.Use(logger.New(logger.Config{
 		// For more options, see the Config section
-		Format:     "${time}\t${pid}\t${locals:requestid}\t${status}\t${method} ${path}​\n",
-		TimeFormat: "2006-01-02T15:04:05",
+		Format:     "${time}|${pid}|${ip}|${port}|${status}|${method}|${path}​\n",
+		TimeFormat: "2006-01-02|15:04:05",
 		TimeZone:   "Asia/Seoul",
-		Output:     file,
+		// Output:     file,
 	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!!!!!")
 	})
 
+	app.Get("/test", func(c *fiber.Ctx) error {
+		return c.Download("./test/test.cmd")
+	})
+
 	app.Get("/metrics", monitor.New(monitor.Config{Title: "MyService Metrics Page"}))
 
-	log.Fatal(app.Listen(":7979"))
+	log.Fatal(app.Listen(":7979")) // http://localhost:7979/
 }
