e701a7cb5c03015be25ec48147149e83767ef15d
ooooo <40694033+wlsfla@users.noreply.github.com>
2022-12-29 17:53:36 +0900

    c# ver dev~ing
    client language migrationing...
    powershell to C#

--- Pol_update/PCinfo_Manager.cs
+++ Pol_update/PCinfo_Manager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,38 +15,54 @@ namespace Pol_update
 	{
 		private RegistryKey reg;
 
-		public PCinfo_Manager()
+		public PCinfo_Manager() : this("localhost:9999")
+		{
+			
+		}
+
+		public PCinfo_Manager(string server_addr)
 		{
 			this.reg = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
-
-			this.ServerAddr = "localhost:9999";
+			this.ServerAddr = server_addr;
+			this.winverList = new Dictionary<string, double>()
+			{
+				{ "1803", 17134.2208},
+				{ "1809", 17763.3653},
+				{ "1903", 18362.1256},
+				{ "1909", 18363.2274},
+				{ "20H2", 19042.2311},
+				{ "21H1", 19043.2311},
+				{ "21H2", 19044.2311}
+			};
 		}
 
-		public string CurrentBuildNumber
+		private Dictionary<string, double> winverList { get; set; }
+
+		private string CurrentBuildNumber
 		{
 			get
 			{
-				return (string)this.reg.GetValue("CurrentBuildNumber");
+				return Convert.ToString(reg.GetValue("CurrentBuildNumber"));
 			}
 		}
 
-		public string UBR
+		private string UBR
 		{
 			get
 			{
-				return (string)this.reg.GetValue("UBR");
+				return Convert.ToString(Convert.ToDouble(this.reg.GetValue("UBR")));
 			}
 		}
 
-		public string BuildVer
+		private double CurrentBuildVer
 		{
 			get
 			{
-				return $"{this.CurrentBuildNumber}.{this.UBR}";
+				return Convert.ToDouble($"{this.CurrentBuildNumber}.{this.UBR}");
 			}
 		}
 
-		public string DisplayVersion
+		private string DisplayVersion
 		{
 			get
 			{
@@ -58,7 +75,7 @@ namespace Pol_update
 			}
 		}
 
-		public string ReleaseId
+		private string ReleaseId
 		{
 			get
 			{
@@ -66,7 +83,7 @@ namespace Pol_update
 			}
 		}
 
-		public string Hostname
+		private string Hostname
 		{
 			get
 			{
@@ -76,12 +93,34 @@ namespace Pol_update
 
 		public string ServerAddr { get; set; }
 
+		private bool IsContainsDpVer
+		{
+			get { return this.winverList.ContainsKey(this.DisplayVersion); }
+		}
+
+		public void ChkBuildVer()
+		{
+			if ((this.DisplayVersion == "1803") || !IsContainsDpVer || (this.CurrentBuildVer >= this.winverList[this.DisplayVersion])) // 1803 버전이거나 현재 버전과 다르면
+			{
+				Console.WriteLine("[info] Cannot Update.");
+				new WebClient().DownloadString($"http://{this.ServerAddr}/api/result/2");
+				return;
+			}
+
+			if (this.CurrentBuildVer == this.winverList[this.DisplayVersion])
+			{
+				Console.WriteLine("[info] Cannot Update.");
+				new WebClient().DownloadString($"http://{this.ServerAddr}/api/result/1");
+				return;
+			}
+
+			Console.WriteLine($"[info] Access This URL. And Download File In Internet Explorer \n\n >>> http://{this.ServerAddr}/file/{this.DisplayVersion}\n");
+		}
+
 		public void SendPcinfo()
 		{
-			string Url = $"http://{this.ServerAddr}/api/info_reg/{this.Hostname}/{this.DisplayVersion}/{this.BuildVer}";
-
+			string Url = $"http://{this.ServerAddr}/api/info_reg/{this.Hostname}/{this.DisplayVersion}/{this.CurrentBuildVer}";
 			new WebClient().DownloadString(Url);
-
 		}
 
 	
--- Pol_update/Program.cs
+++ Pol_update/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
+using System.
 namespace Pol_update
 {
 	internal class Program
@@ -12,28 +15,30 @@ namespace Pol_update
 		static void Main(string[] args)
 		{
 			Console.WriteLine("[info] Starting Windows Security Update...");
-
+			PCinfo_Manager info = new PCinfo_Manager();
+			if (args.Length == 1)
+			{
+				info.ServerAddr = args[0];
+			}
+			
 			// polupdate 경로 생성
 			//SetPolUpdatePath();
 
+
 			// add Task schd
-			//AddTaskSchd();
+			AddTaskSchd();
 
 			// update.ps1
-			Console.WriteLine(
-				new PCinfo_Manager().ServerAddr = ""
-			);
+			//info.SendPcinfo();
+			//info.ChkBuildVer();
 
-
-			//Console.WriteLine("End");
+			Console.WriteLine("[info] 업데이트가 끝났습니다. 아무 키나 누르면 창이 종료됩니다.");
 			//Console.ReadKey();
 		}
 
 		static void AddTaskSchd()
 		{
-			// add Task.ps1
-			ExecutePowerShellScript(
-				@"if  (!(Test-Path ""C:\Windows\System32\Tasks\pol_windows_update"")) {
+			string cmd = @"if (!(Test-Path ""C:\Windows\System32\Tasks\pol_windows_update"")) {
 					$action = New-ScheduledTaskAction -Execute ""$($env:userprofile+'\polupdate\win_update_check.exe')"" -Argument ""10.16.38.21:9999""
 					$t1 = New-ScheduledTaskTrigger -Daily -At ""09:00"" -RandomDelay (New-TimeSpan -Hours 6)
 					$t1.EndBoundary = ""2023-01-13T18:00:00""
@@ -41,19 +46,42 @@ namespace Pol_update
 					$t2.EndBoundary = ""2023-01-13T18:00:00""
 
 					[void](Register-ScheduledTask pol_windows_update -Action $action -Trigger @($t1, $t2))
-				}"
-			);
+				}";
+			StringBuilder cmd2 = new StringBuilder();
+			cmd2.Append(@"if (!(Test-Path ""C:\Windows\System32\Tasks\pol_windows_update"")) {");
+			cmd2.Append("$action = New-ScheduledTaskAction -Execute ");
+			cmd2.Append("$action = New-ScheduledTaskAction -Execute ");
+
+			// C:\\Windows\\System32\\Tasks\\pol_windows_update
+			string cmd2 = $"$action = New-ScheduledTaskAction -Execute \"$($env:userprofile+'\\polupdate\\win_update_check.exe')\" -Argument \"10.16.38.21:9999\"
+				
+				$t1 = New-ScheduledTaskTrigger -Daily -At ""09:00"" -RandomDelay (New-TimeSpan -Hours 6)
+				$t1.EndBoundary = ""2023-01-13T18:00:00""
+				$t2 = New-ScheduledTaskTrigger -AtLogOn
+				$t2.EndBoundary = ""2023-01-13T18:00:00""
+
+					[void](Register-ScheduledTask pol_windows_update -Action $action -Trigger @($t1, $t2))
+				}";
+
+			Console.WriteLine(cmd2);
+			//ExecutePowerShellScript(cmd);
 		}
 
 		static void SetPolUpdatePath()
 		{
-			ExecutePowerShellScript(
-				@"[void](New-Item -Path $($env:userprofile+'\polupdate') -ItemType Directory -Force)"
-			);
+			string polPath = Path.Combine(
+				Environment.GetEnvironmentVariable("userprofile"),
+				"polupdate");
 
-			ExecutePowerShellScript(
-				@"Copy-Item -Force .\update\win_update_check.exe $($env:userprofile+'\polupdate')"
-			);
+			// 디렉토리 없으면 만들고
+			if (!Directory.Exists(polPath))
+				Directory.CreateDirectory(polPath);
+
+			//Application.StartupPath
+			// 파일 없으면 복사
+			string targetPath = Path.Combine(polPath, Path.GetFileName(Application.ExecutablePath));
+			if (!File.Exists(targetPath))
+				File.Copy(Application.ExecutablePath, targetPath);
 		}
 
 		static void ExecutePowerShellScript(string script)
@@ -61,7 +89,7 @@ namespace Pol_update
 			ProcessStartInfo processStartInfo = new ProcessStartInfo()
 			{
 				FileName = "powershell.exe",
-				Arguments = $"-nop -ep unrestricted {script}",
+				Arguments = $"-ep unrestricted {script}",
 				UseShellExecute = false
 			};
 
