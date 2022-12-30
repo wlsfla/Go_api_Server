24f088eea539f4d1566f4333eab4a3f6b456b701
ooooo <40694033+wlsfla@users.noreply.github.com>
2022-12-30 18:05:21 +0900

    Dev TaskScheduler in C#
    Abrogate Powershell Lib

---
+++ Pol_update/Common.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pol_update
+{
+	internal static class Common
+	{
+		/// <summary>
+		/// %userprofile%\polupdate 폴더 경로
+		/// </summary>
+		public static string PolPath { get; set; }
+
+		/// <summary>
+		/// 현재 실행 파일 전체 경로
+		/// </summary>
+		public static string PolExecPath { get; set; }
+
+		/// <summary>
+		/// 현재 실행 파일 전체 경로
+		/// </summary>
+		public static string NowExecPath
+		{
+			get { return System.Windows.Forms.Application.ExecutablePath; }
+		}
+
+		/// <summary>
+		/// 현재 실행 파일 폴더
+		/// </summary>
+		public static string NowExecDir
+		{
+			get { return System.Windows.Forms.Application.StartupPath; }
+		}
+
+		static Common()
+		{
+			PolPath = Path.Combine(Environment.GetEnvironmentVariable("userprofile"), "polupdate");
+			PolExecPath = Path.Combine(PolPath, Path.GetDirectoryName(NowExecPath));
+
+			//Path.GetFileName(Application.ExecutablePath)
+		}
+	}
+}
--- Pol_update/Program.cs
+++ Pol_update/Program.cs
@@ -2,12 +2,9 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.
+
 namespace Pol_update
 {
 	internal class Program
@@ -20,20 +17,21 @@ namespace Pol_update
 			{
 				info.ServerAddr = args[0];
 			}
-			
+
+			TaskScheduleHandler.test(@"\PolUpdate");
+
 			// polupdate 경로 생성
 			//SetPolUpdatePath();
 
-
 			// add Task schd
-			AddTaskSchd();
+			//AddTaskSchd();
 
 			// update.ps1
 			//info.SendPcinfo();
 			//info.ChkBuildVer();
 
 			Console.WriteLine("[info] 업데이트가 끝났습니다. 아무 키나 누르면 창이 종료됩니다.");
-			//Console.ReadKey();
+			Console.ReadKey();
 		}
 
 		static void AddTaskSchd()
@@ -53,35 +51,19 @@ namespace Pol_update
 			cmd2.Append("$action = New-ScheduledTaskAction -Execute ");
 
 			// C:\\Windows\\System32\\Tasks\\pol_windows_update
-			string cmd2 = $"$action = New-ScheduledTaskAction -Execute \"$($env:userprofile+'\\polupdate\\win_update_check.exe')\" -Argument \"10.16.38.21:9999\"
-				
-				$t1 = New-ScheduledTaskTrigger -Daily -At ""09:00"" -RandomDelay (New-TimeSpan -Hours 6)
-				$t1.EndBoundary = ""2023-01-13T18:00:00""
-				$t2 = New-ScheduledTaskTrigger -AtLogOn
-				$t2.EndBoundary = ""2023-01-13T18:00:00""
-
-					[void](Register-ScheduledTask pol_windows_update -Action $action -Trigger @($t1, $t2))
-				}";
-
-			Console.WriteLine(cmd2);
+			
 			//ExecutePowerShellScript(cmd);
 		}
 
 		static void SetPolUpdatePath()
 		{
-			string polPath = Path.Combine(
-				Environment.GetEnvironmentVariable("userprofile"),
-				"polupdate");
-
 			// 디렉토리 없으면 만들고
-			if (!Directory.Exists(polPath))
-				Directory.CreateDirectory(polPath);
-
-			//Application.StartupPath
+			if (!Directory.Exists(Common.PolPath))
+				Directory.CreateDirectory(Common.PolPath);
+			
 			// 파일 없으면 복사
-			string targetPath = Path.Combine(polPath, Path.GetFileName(Application.ExecutablePath));
-			if (!File.Exists(targetPath))
-				File.Copy(Application.ExecutablePath, targetPath);
+			if (!File.Exists(Common.PolExecPath))
+				File.Copy(Common.NowExecPath, Common.PolExecPath);
 		}
 
 		static void ExecutePowerShellScript(string script)
